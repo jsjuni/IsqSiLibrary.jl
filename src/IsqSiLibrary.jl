e87ca50d864a5acc732d80cb4244b568375f33e0
James Steven Jenkins <sjenkins@studioj.us>
2026-08-16 00:49:47 -0400

    first working version

--- src/IsqSiLibrary.jl
+++ src/IsqSiLibrary.jl
@@ -1,11 +1,168 @@
 module IsqSiLibrary
 
     using CSV
+    using OrderedCollections
+    using DataFrames
+    using NamingConventions
  
-    export parse_csv_source
+    using NamingConventions
+
+    const ONTOLOGY_TYPES = Dict(
+        "vocabulary" => "v",
+        "description" => "d"
+    )
+
+    const ONTOLOGY_COMPANION = Dict(
+        "v" => "d",
+        "d" => "v"
+    )
+
+    const BUNDLE_TYPES = Dict(
+        "vocabulary bundle" => "vb",
+        "description bundle" => "db"
+    )
     
-    function parse_csv_source(filename)
-        CSV.File(filename)
+    const BUNDLE_IMPORTS = Dict(
+        "vocabulary bundle" => ["vocabulary"],
+        "description bundle" => ["vocabulary bundle", "description"]
+    )
+
+    struct SpaceCase <: AbstractNamingConvention end
+
+    function NamingConventions.encode(::Type{SpaceCase}, v::AbstractString)
+        return join(v, " ")
+    end
+
+    function NamingConventions.decode(::Type{SpaceCase}, s::AbstractString)
+        return split(s)
+    end
+
+    function instance_name(string)
+        NamingConventions.convert(SpaceCase, SnakeCase, string)
+    end
+
+    function remove_paren_text(string)
+        replace(string, r" \(.*" => "")
+    end
+
+    export parse_csv_source
+    function parse_csv_source(path)
+        CSV.read(path, DataFrame)
+    end
+
+    export table_to_dict
+    function table_to_dict(table, key)
+        sub_keys = filter(n -> n != key, names(table))
+        d = OrderedDict{String, OrderedDict{String, Any}}()
+        for row in eachrow(table)
+            id = OrderedDict()
+            for sub_key in sub_keys
+                id[sub_key] = row[sub_key]
+            end
+            d[row[key]] = id
+        end
+        d
+    end
+
+    export initialize_dictionary
+    function initialize_dictionary()
+        OrderedDict{String, Any}()
+    end
+
+    export construct_ontologies
+    function construct_ontologies(documents)
+
+        ontologies = initialize_dictionary()
+        for (document, document_data) in documents
+            m = match(r"^(ISO|IEC) 80000-(\d+)$", document)
+            if !isnothing(m)
+                for (type, suffix) in ONTOLOGY_TYPES
+                    d = initialize_dictionary()
+                    d["source"] = document
+                    d["type"] = type
+                    d["iri_stem"] = "$(lowercase(m[1]))-80000/$(m[2])-$suffix"
+                    d["prefix"] = "$(lowercase(m[1]))-80000-$(m[2])-$suffix"
+                    d["companion_iri_stem"] = replace(d["iri_stem"], Regex("-$suffix\$") => "-$(ONTOLOGY_COMPANION[suffix])")
+                    ontologies["$document $type"] = d
+                end
+            end
+        end
+        ontologies
+    end
+
+    export construct_bundles
+    function construct_bundles(ontologies, stem = "iso-80000")
+
+        bundles = initialize_dictionary()
+        for (type, suffix) in BUNDLE_TYPES
+            d = initialize_dictionary()
+            d["type"] = type
+            d["iri_stem"] = "$stem/$suffix"
+           bundles["$stem $type"] = d
+        end
+        for (bundle_data) in values(bundles)
+            imports = []
+            imports_type_list = BUNDLE_IMPORTS[bundle_data["type"]]
+            for import_type in imports_type_list
+                candidates = collect(Iterators.flatmap(h -> values(h), [bundles, ontologies]))
+                append!(imports, map(oh -> oh["iri_stem"], filter(oh -> oh["type"] == import_type, candidates)))
+            end
+            bundle_data["imports"] = imports
+        end
+        bundles
+    end
+
+    export construct_quantity_instances
+    function construct_quantity_instances(quantities, ontologies)
+        quantity_instances = initialize_dictionary()
+        for (quantity, quantity_data) in quantities
+            d = initialize_dictionary()
+            d["name"] = quantity
+            document = remove_paren_text(quantity_data["Defining Document"])
+            ontology_key = "$document description"
+            ontology_data = ontologies[ontology_key]
+            d["description_iri_stem"] = ontology_data["iri_stem"]
+            d["vocabulary_iri_stem"] = ontology_data["companion_iri_stem"]
+            d["type"] = quantity_data["Type"]
+            d["description"] = quantity_data["Description"]
+            d["symbol"] = quantity_data["Symbol"]
+            d["dimension_symbol"] = quantity_data["Dimension Symbol"]
+            name = instance_name(quantity)
+            d["kind_class"] = NamingConventions.convert(SnakeCase, PascalCase, name)
+            d["unit_class"] = "$(d["kind_class"])Unit"
+            d["value_class"] = "$(d["kind_class"])Value"
+            quantity_instances[name] = d
+        end
+        quantity_instances
+    end
+
+    export construct_unit_instances
+    function construct_unit_instances(units, quantities, ontologies, quantity_instances)
+        unit_instances = initialize_dictionary()
+        for (unit, unit_data) in units
+            d = initialize_dictionary()
+            d["name"] = unit
+            document = remove_paren_text(unit_data["Defining Document"])
+            ontology_key = "$document description"
+            ontology_data = ontologies[ontology_key]
+            d["description_iri_stem"] = ontology_data["iri_stem"]
+            d["vocabulary_iri_stem"] = ontology_data["companion_iri_stem"]
+            d["type"] = unit_data["Type"]
+            if d["type"] == "Derived"
+                d["expression"] = unit_data["Expressed In Base Units"]
+            end
+            d["definition"] = unit_data["Definition"]
+            d["symbol"] = unit_data["Symbol"]
+            quantity_string = unit_data["ISQ Quantities"]
+            if !ismissing(quantity_string)
+                quantity = instance_name(remove_paren_text(quantity_string))
+                d["quantity"] = quantity
+                d["unit_class"] = NamingConventions.convert(SnakeCase, PascalCase, quantity)
+            end
+            name = instance_name(unit)
+            unit_instances[name] = d
+        end
+        unit_instances
     end
 
 end
--- src/ProcessSources.jl
+++ src/ProcessSources.jl
@@ -4,6 +4,7 @@ module Main
     using Logging
     using Dates
     using IsqSiLibrary
+    using JSON
 
     function parse_commandline()
         s = ArgParseSettings()
@@ -24,30 +25,95 @@ module Main
                 help = "units source file (JSON)"
                 arg_type = String
                 required = true
+            "--prefixes-source"
+                help = "prefixes source file (JSON)"
+                arg_type = String
+                required = true
         end
         return parse_args(s)
     end
 
+    function load_csv_document(prefix, path)
+       parse_csv_source(joinpath(prefix, path))
+    end
+
     function (@main)(ARGS)
-         
+
+        knowledge = initialize_dictionary()
+
         @info "$(now()) start"
         @info "$(now()) parse command arguments"
         args = parse_commandline()
     
+        #
+        # parse input data sources
+        #
+
         @info "$(now()) loading documents source $(args["documents-source"])"
-        docs_source = joinpath(args["sources-path-prefix"], args["documents-source"])
-        documents_csv = parse_csv_source(docs_source)
-        @info documents_csv
+        documents_csv = load_csv_document(args["sources-path-prefix"], args["documents-source"])
+ 
+        @info "$(now()) loading quantities source $(args["quantities-source"])"
+        quantities_csv = load_csv_document(args["sources-path-prefix"], args["quantities-source"])
 
-        @info "$(now()) loading quantities source $(args["documents-source"])"
-        docs_source = joinpath(args["sources-path-prefix"], args["quantities-source"])
-        quantities_csv = parse_csv_source(docs_source)
-        @info quantities_csv
+        @info "$(now()) loading units source $(args["units-source"])"
+        units_csv = load_csv_document(args["sources-path-prefix"], args["units-source"])
 
-        @info "$(now()) loading units source $(args["documents-source"])"
-        docs_source = joinpath(args["sources-path-prefix"], args["units-source"])
-        units_csv = parse_csv_source(docs_source)
-        @info units_csv
+        @info "$(now()) loading prefixes source $(args["prefixes-source"])"
+        prefixes_csv = load_csv_document(args["sources-path-prefix"], args["prefixes-source"])
+
+        #
+        # build dictionaries of input data
+        #
+
+        @info "$(now()) building documents dictionary"
+        documents = table_to_dict(documents_csv, "Document")
+ 
+        @info "$(now()) building quantities dictionary"
+        quantities = table_to_dict(quantities_csv, "Quantity")
+
+        @info "$(now()) building units dictionary"
+        units = table_to_dict(units_csv, "Unit")
+
+        @info "$(now()) building prefixes dictionary"
+        prefixes = table_to_dict(prefixes_csv, "Prefix")
+
+        #
+        # create ontologies and bundles
+        #
+
+        @info "$(now()) create ontologies"
+        ontologies = construct_ontologies(documents)
+        knowledge["ontologies"] = ontologies
+
+        @info "$(now()) create bundles"
+        bundles = construct_bundles(ontologies)
+        knowledge["bundles"] = bundles
+
+        #
+        # create quantity instances
+        #
+
+        @info "$(now()) create quantity instances"
+        quantity_instances = construct_quantity_instances(quantities, ontologies)
+        knowledge["quantity_instances"] = quantity_instances
+
+        #
+        # create unit instances
+        #
+
+        @info "$(now()) create unit instances"
+        unit_instances = construct_unit_instances(units, quantities, ontologies, quantity_instances)
+        knowledge["unit_instances"] = unit_instances
+
+        #
+        # write output
+        #
+
+        JSON.json(stdout, knowledge, pretty = true)
+        
+        #
+        # end
+        #
 
         @info "$(now()) end"
     end
